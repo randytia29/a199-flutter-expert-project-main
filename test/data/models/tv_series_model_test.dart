446002ff1fcc349370515941a11fe1335527ba34
Randytia Akbar <randytia.akbar@gmail.com>
2023-05-11 08:19:43 +0700

    add on air json

---
+++ test/data/models/tv_series_model_test.dart
@@ -0,0 +1,32 @@
+import 'package:ditonton/data/models/tv_series_model.dart';
+import 'package:ditonton/domain/entities/tv_series.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  final tTvSeriesModel = TvSeriesModel(
+    backdropPath: 'backdropPath',
+    genreIds: [1, 2, 3],
+    id: 1,
+    overview: 'overview',
+    popularity: 1,
+    posterPath: 'posterPath',
+    voteAverage: 1,
+    voteCount: 1,
+  );
+
+  final tTvSeries = TvSeries(
+    backdropPath: 'backdropPath',
+    genreIds: [1, 2, 3],
+    id: 1,
+    overview: 'overview',
+    popularity: 1,
+    posterPath: 'posterPath',
+    voteAverage: 1,
+    voteCount: 1,
+  );
+
+  test('should be a subclass of tv series entity', () async {
+    final result = tTvSeriesModel.toEntity();
+    expect(result, tTvSeries);
+  });
+}
---
+++ test/data/models/tv_series_response_model_test.dart
@@ -0,0 +1,67 @@
+import 'dart:convert';
+
+import 'package:ditonton/data/models/tv_series_model.dart';
+import 'package:ditonton/data/models/tv_series_response.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../json_reader.dart';
+
+void main() {
+  final tTvSeriesModel = TvSeriesModel(
+    backdropPath: "/path.jpg",
+    genreIds: [1, 2, 3, 4],
+    id: 1,
+    overview: "Overview",
+    popularity: 1.0,
+    posterPath: "/path.jpg",
+    voteAverage: 1.0,
+    voteCount: 1,
+    name: 'name',
+    originCountry: ['en'],
+    originalLanguage: 'originalLanguage',
+    originalName: 'originalName',
+  );
+
+  final tTvSeriesResponseModel =
+      TvSeriesResponse(tvSeriesList: <TvSeriesModel>[tTvSeriesModel]);
+  group('fromJson', () {
+    test('should return a valid tv series from JSON', () async {
+      // arrange
+      final Map<String, dynamic> jsonMap =
+          json.decode(readJson('dummy_data/on_the_air.json'));
+      // act
+      final result = TvSeriesResponse.fromJson(jsonMap);
+      // assert
+      expect(result, tTvSeriesResponseModel);
+    });
+  });
+
+  group('toJson', () {
+    test('should return a JSON map containing proper data', () async {
+      // arrange
+
+      // act
+      final result = tTvSeriesResponseModel.toJson();
+      // assert
+      final expectedJsonMap = {
+        "results": [
+          {
+            "poster_path": "/path.jpg",
+            "popularity": 1.0,
+            "id": 1,
+            "backdrop_path": "/path.jpg",
+            "vote_average": 1,
+            "overview": "Overview",
+            "origin_country": ["en"],
+            "genre_ids": [1, 2, 3, 4],
+            "original_language": "originalLanguage",
+            "vote_count": 1,
+            "name": "name",
+            "original_name": "originalName"
+          }
+        ],
+      };
+      expect(result, expectedJsonMap);
+    });
+  });
+}
